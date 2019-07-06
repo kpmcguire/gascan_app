c1b237cb15cd6f81d1e1e8ae1671cffbfe8cc50b
Kevin McGuire <kevin@givegab.com>
2019-07-06 17:21:11 -0400

    trying to get it working on heroku

--- gascan/gascan/settings.py
+++ gascan/gascan/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
